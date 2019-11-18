9fa783f995b57cbeccbc27aa7a535521513015a4
pusztaienike <eniko.pusztai@outlook.com>
2019-11-18 11:27:41 +0100

    main components have created

---
+++ src/components/Card.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="card">
+        <div>{{ item.title }}</div>
+        <div><img :src="item.thumbnail" /></div>
+        <div>{{ item.description }}</div>
+        <div
+            class="label-container"
+            v-for="(label, index) in item.labels"
+            :key="index"
+        >
+            <label-button :label="label"></label-button>
+        </div>
+    </div>
+</template>
+
+<script>
+import LabelButton from './LabelButton';
+
+export default {
+    name: 'Card',
+    props: {
+        item: {
+            type: Object,
+            default: null
+        }
+    },
+    components: {
+        'label-button': LabelButton
+    }
+};
+</script>
+
+<style scoped lang="scss">
+.card {
+    img {
+        width: 50%;
+        height: 50%;
+    }
+}
+</style>
---
+++ src/components/LabelButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="label-button">
+        {{ label }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LabelButton',
+    props: {
+        label: {
+            type: String,
+            default: ''
+        }
+    }
+};
+</script>
+
+<style scoped lang="scss"></style>
